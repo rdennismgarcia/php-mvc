32e99158809f72bbc196af6a77efa5175e2db191
Dennis Garcia <33927319+rdennismgarcia@users.noreply.github.com>
2025-10-07 23:49:43 +0800

    Add files via upload

---
+++ app/helpers/session_helper.php
@@ -0,0 +1,41 @@
+<?php
+
+ini_set('session.gc_maxlifetime', 86400);
+ini_set('session.cookie_lifetime', 86400);
+session_start();
+// flash message helper
+// Example - flash('register_success', 'you are registered');
+//Display in view -<?= flash(register_success'); 
+function flash($name = '', $message = '', $class = 'alert alert-success')
+{
+    if (!empty($name)) {
+        if (!empty($message) && empty($_SESSION[$name])) {
+            // $_SESSION[$name] = $name;
+            if (!empty($_SESSION[$name])) {
+                unset($_SESSION[$name]);
+            }
+
+
+
+            if (!empty($_SESSION[$name . '_class'])) {
+                unset($_SESSION[$name . '_class']);
+            }
+
+            $_SESSION[$name] = $message;
+            $_SESSION[$name . '_class'] = $class;
+        } elseif (empty($message) && !empty($_SESSION[$name])) {
+            $class = !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : '';
+            echo '<div class="' . $class . '" id="msg-flash">' . $_SESSION[$name] . '</div>';
+            unset($_SESSION[$name]);
+            unset($_SESSION[$name . '_class']);
+        }
+    }
+}
+function isLoggedIn()
+{
+    if (isset($_SESSION['user_id'])) {
+        return true;
+    } else {
+        return false;
+    }
+}
