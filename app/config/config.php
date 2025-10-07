d6aa2ad1ed29b717d9609c6a0b17e988d9180c61
Dennis Garcia <33927319+rdennismgarcia@users.noreply.github.com>
2025-10-07 23:50:51 +0800

    Create config.php

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+//App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+define('URLROOT', 'https://'.$_SERVER['HTTP_HOST'].'/');
+//site name
+define('SITENAME', '');
