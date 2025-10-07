1bec860bd8525edf3bf773821e1fd77cfdcf1b7e
Dennis Garcia <33927319+rdennismgarcia@users.noreply.github.com>
2025-10-07 23:59:07 +0800

    Create home.php

---
+++ app/views/pages/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP MVC Framework</title>
+  </head>
+  <body>
+    <h2>PHP MVC Framework</h2>
+    <p>Developed by Dennis Garcia</p>
+    <p>Your MVC framework has been successfully set up. Seeing this page confirms that your setup is complete and functioning as expected.</p>
+  </body>
+</html>
